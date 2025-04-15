354050fb5e0c89bea495e5707569709c82fc91fa
Vasu Ganesh <vasu.ganesh09@gmail.com>
2025-04-14 19:51:13 -0500

    More data

--- preprocessing.py
+++ preprocessing.py
@@ -2,14 +2,25 @@ import pandas as pd
 
 
 def load_data(name):
-    return pd.read_csv(f"data/{name}.csv")
+    df = pd.read_csv(f"data/{name}.csv")
+    
+    # Drop unnecessary columns
+    df = df.drop(columns=["STATION", "WV01", "WV03"], errors="ignore")
+
+    # Shorten name column (WIP)
+    df["NAME"] = df["NAME"].str.split().str[0]
+
+    return df
 
 
-def separate_stations():
-    pass
+def separate_stations(df):
+    station_groups = df.groupby("NAME")
+
+    return station_groups
 
 
 if __name__ == "__main__":
     df1 = load_data("CMCTB")
 
-    print(df1.head())
+    print(df1)
+    #separate_stations(df1)
